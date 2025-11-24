da8054e08e75b72bb18c3342f01dc6a009e1e017
cx-happy-yang <30431255+cx-happy-yang@users.noreply.github.com>
2025-11-24 12:49:40 +0800

    [Update] add parameter delay_request_timeout to function batch_import_repo

--- main.py
+++ main.py
@@ -85,5 +85,6 @@ if __name__ == '__main__':
             is_user=False, is_org_webhook_enabled=False, create_ast_project=True, scan_ast_project=False,
             auth_code=cxone_github_auth_code,
             webhook_enabled=False, pr_decoration_enabled=False, sca_auto_pr_enabled=False,
-            chunk_size=200
+            chunk_size=200,
+            delay_request_timeout=30,
         )
