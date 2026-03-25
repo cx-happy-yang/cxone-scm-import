d6276c342be57bcc8f761c17188c9af4c575edc0
cx-happy-yang <30431255+cx-happy-yang@users.noreply.github.com>
2026-03-26 00:19:08 +0800

    [Add] github_app_import.py

---
+++ github_app_import.py
@@ -0,0 +1,98 @@
+from dotenv import load_dotenv
+import os
+import logging
+load_dotenv("./happy.env", override=True)
+from github import (Github, Auth)
+from CheckmarxPythonSDK.CxOne import (
+    RepoManagerAPI,
+    batch_import_repo,
+    get_all_projects,
+    get_a_project_by_id,
+    get_repo_by_id,
+    update_repo_by_id,
+)
+
+logger = logging.getLogger("CheckmarxPythonSDK")
+
+
+def get_github_repos_by_org(organization: str, access_token: str):
+    # using an access token
+    auth = Auth.Token(access_token)
+    # Public Web Github
+    g = Github(auth=auth)
+    org = g.get_organization(organization)
+    return org.get_repos()
+
+
+if __name__ == '__main__':
+    github_org = os.getenv("GITHUB_ORG")
+    logger.info(f"github organization: {github_org}")
+    github_access_token = os.getenv("GITHUB_TOKEN")
+    cxone_github_app_auth_code = os.getenv("CXONE_GITHUBAPP_AUTH_CODE")
+    repos_from_github = get_github_repos_by_org(organization=github_org, access_token=github_access_token)
+    project_list = get_all_projects()
+    project_name_list = [project.name for project in project_list]
+    project_id_list = [project.id for project in project_list]
+
+    repos = []
+    for repo in repos_from_github:
+        html_url = repo.html_url
+        branch = repo.default_branch
+        org_repo_name = html_url.replace("https://github.com/", "")
+        repo_name = org_repo_name.split("/")[1]
+        if org_repo_name in project_name_list:
+            logger.info(f"repo {org_repo_name} already exist in cx one, update repo settings")
+            index = project_name_list.index(org_repo_name)
+            project_id = project_id_list[index]
+            try:
+                logger.info("get a project by id")
+                project = get_a_project_by_id(project_id=project_id)
+                logger.info("get a repo by id")
+                repo = get_repo_by_id(repo_id=project.repo_id)
+                logger.info(f"turn on project {project.name} scaAutoPrEnabled")
+                update_repo_by_id(repo_id=project.repo_id, project_id=project_id, pay_load={
+                    "branches": repo.get("branches"),
+                    "kicsScannerEnabled": repo.get("kicsScannerEnabled"),
+                    "sastIncrementalScan": repo.get("sastIncrementalScan"),
+                    "sastScannerEnabled": repo.get("sastScannerEnabled"),
+                    "scaScannerEnabled": repo.get("scaScannerEnabled"),
+                    "apiSecScannerEnabled": repo.get("apiSecScannerEnabled"),
+                    "url": repo.get("url"),
+                    "webhookEnabled": repo.get("webhookEnabled"),
+                    "prDecorationEnabled": repo.get("prDecorationEnabled"),
+                    "secretsDetectionScannerEnabled": repo.get("secretsDetectionScannerEnabled"),
+                    "ossfScoreCardScannerEnabled": repo.get("ossfScoreCardScannerEnabled"),
+                    "scaAutoPrEnabled": True,
+                    "webhookId": repo.get("webhookId"),
+                    "sshRepoUrl": repo.get("sshRepoUrl"),
+                    "sshState": "SKIPPED",
+                    "isRepoAdmin": repo.get("isRepoAdmin"),
+                    "containerScannerEnabled": repo.get("containerScannerEnabled")
+                })
+            except Exception:
+                logger.info(f"Failed to update project {org_repo_name} repo settings")
+                continue
+        else:
+            repos.append({
+                "id": repo_name,
+                "fullName": org_repo_name,
+                "url": html_url,
+                "sshRepoUrl": f"git@github.com:{org_repo_name}.git",
+                "defaultBranch": repo.default_branch,
+            })
+    if github_org and cxone_github_app_auth_code:
+        batch_import_repo(
+            repos=repos, 
+            origin="GITHUB", 
+            organization=github_org,
+            is_user=False, 
+            is_org_webhook_enabled=False, 
+            create_ast_project=True, 
+            scan_ast_project=False,
+            auth_code=cxone_github_app_auth_code,
+            webhook_enabled=False, 
+            pr_decoration_enabled=False, 
+            sca_auto_pr_enabled=False,
+            chunk_size=200,
+            delay_request_timeout=30,
+        )
